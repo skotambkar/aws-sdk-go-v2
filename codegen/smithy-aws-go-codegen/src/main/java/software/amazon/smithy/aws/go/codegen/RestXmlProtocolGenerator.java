6bdc9afe16611fb793d75ac21f88392abd8cb7bd
skotambkar <kotambka@amazon.com>
2020-07-19 14:49:47 -0700

    xml serialization visitor implementation

--- codegen/smithy-aws-go-codegen/src/main/java/software/amazon/smithy/aws/go/codegen/RestXmlProtocolGenerator.java
+++ codegen/smithy-aws-go-codegen/src/main/java/software/amazon/smithy/aws/go/codegen/RestXmlProtocolGenerator.java
@@ -1,11 +1,15 @@
 package software.amazon.smithy.aws.go.codegen;
 
+import static software.amazon.smithy.go.codegen.integration.ProtocolUtils.writeSafeMemberAccessor;
+
+import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
-import software.amazon.smithy.codegen.core.SymbolProvider;
 import software.amazon.smithy.go.codegen.GoStackStepMiddlewareGenerator;
 import software.amazon.smithy.go.codegen.GoWriter;
+import software.amazon.smithy.go.codegen.SmithyGoDependency;
 import software.amazon.smithy.go.codegen.integration.HttpBindingProtocolGenerator;
+import software.amazon.smithy.go.codegen.integration.ProtocolGenerator;
 import software.amazon.smithy.go.codegen.integration.ProtocolUtils;
 import software.amazon.smithy.model.Model;
 import software.amazon.smithy.model.knowledge.HttpBinding;
@@ -15,6 +19,8 @@ import software.amazon.smithy.model.shapes.OperationShape;
 import software.amazon.smithy.model.shapes.Shape;
 import software.amazon.smithy.model.shapes.ShapeId;
 import software.amazon.smithy.model.shapes.StructureShape;
+import software.amazon.smithy.model.traits.MediaTypeTrait;
+import software.amazon.smithy.model.traits.StreamingTrait;
 import software.amazon.smithy.model.traits.TimestampFormatTrait;
 
 abstract class RestXmlProtocolGenerator extends HttpBindingProtocolGenerator {
@@ -25,6 +31,12 @@ abstract class RestXmlProtocolGenerator extends HttpBindingProtocolGenerator {
         super(true);
     }
 
+
+    @Override
+    public void generateSharedDeserializerComponents(GenerationContext context) {
+        super.generateSharedDeserializerComponents(context);
+    }
+
     @Override
     public void generateProtocolTests(GenerationContext context) {
         AwsProtocolUtils.generateHttpProtocolTests(context);
@@ -32,7 +44,7 @@ abstract class RestXmlProtocolGenerator extends HttpBindingProtocolGenerator {
 
     @Override
     protected TimestampFormatTrait.Format getDocumentTimestampFormat() {
-        return null;
+        return TimestampFormatTrait.Format.DATE_TIME;
     }
 
     @Override
@@ -40,8 +52,23 @@ abstract class RestXmlProtocolGenerator extends HttpBindingProtocolGenerator {
         return null;
     }
 
+
     @Override
     protected void generateOperationDocumentSerializer(GenerationContext context, OperationShape operation) {
+        Model model = context.getModel();
+        HttpBindingIndex bindingIndex = model.getKnowledge(HttpBindingIndex.class);
+
+        Set<MemberShape> documentBindings = bindingIndex.getRequestBindings(operation, HttpBinding.Location.DOCUMENT)
+                .stream()
+                .map(HttpBinding::getMember)
+                .collect(Collectors.toSet());
+
+        if (documentBindings.size() == 0) {
+            return;
+        }
+
+        Shape inputShape = ProtocolUtils.expectInput(context.getModel(), operation);
+        inputShape.accept(new XmlShapeSerVisitor(context, documentBindings::contains));
 
     }
 
@@ -51,33 +78,32 @@ abstract class RestXmlProtocolGenerator extends HttpBindingProtocolGenerator {
             OperationShape operation,
             GoStackStepMiddlewareGenerator generator
     ) {
+        GoWriter writer = context.getWriter();
+        writer.addUseImports(SmithyGoDependency.SMITHY);
+        writer.addUseImports(SmithyGoDependency.SMITHY_XML);
 
+        writer.write("restEncoder.SetHeader(\"Content-Type\").String($S)", getDocumentContentType());
+        writer.write("");
+
+        Shape inputShape = ProtocolUtils.expectInput(context.getModel(), operation);
+        String functionName = ProtocolGenerator.getDocumentSerializerFunctionName(inputShape, getProtocolName());
+
+        writer.addUseImports(SmithyGoDependency.SMITHY_XML);
+        writer.write("xmlEncoder := smithyxml.NewEncoder()");
+
+        writer.openBlock("if err := $L(input, xmlEncoder.Value.RootElement().Key($S)); err != nil {", "}",
+                functionName, inputShape.getId().getName(), () -> {
+            writer.write("return out, metadata, &smithy.SerializationError{Err: err}");
+        });
+        writer.write("");
+
+        writer.addUseImports(SmithyGoDependency.BYTES);
+        writer.openBlock("if request, err = request.SetStream(bytes.NewReader(xmlEncoder.Bytes())); err != nil {", "}",
+                () -> {
+                    writer.write("return out, metadata, &smithy.SerializationError{Err: err}");
+                });
     }
 
-    @Override
-    protected void writeErrorMessageCodeDeserializer(GenerationContext context) {
-        context.getWriter().writeDocs("TODO: implement error message / code deser");
-    }
-
-    @Override
-    protected void deserializeError(GenerationContext context, StructureShape shape) {
-        context.getWriter().write("return &smithy.DeserializationError{"
-                + "Err: fmt.Errorf(\"TODO: Implement error deserializer delegators\")}");
-    }
-
-    @Override
-    protected void writeMiddlewareDocumentDeserializerDelegator(
-            GenerationContext context,
-            OperationShape operation,
-            GoStackStepMiddlewareGenerator generator
-    ) {
-        Model model = context.getModel();
-        HttpBindingIndex bindingIndex = model.getKnowledge(HttpBindingIndex.class);
-
-        Set<MemberShape> documentBindings = bindingIndex.getRequestBindings(operation, HttpBinding.Location.DOCUMENT)
-                .stream()
-                .map(HttpBinding::getMember)
-                .collect(Collectors.toSet());
 
     @Override
     protected void writeMiddlewarePayloadSerializerDelegator(
@@ -86,9 +112,44 @@ abstract class RestXmlProtocolGenerator extends HttpBindingProtocolGenerator {
             MemberShape memberShape,
             GoStackStepMiddlewareGenerator generator
     ) {
+        GoWriter writer = context.getWriter();
+        Model model = context.getModel();
+        Shape payloadShape = model.expectShape(memberShape.getTarget());
 
-        Shape inputShape = ProtocolUtils.expectInput(model, operation);
-        inputShape.accept(new XmlShapeSerVisitor(context, documentBindings::contains));
+        writeSafeMemberAccessor(context, memberShape, "input", s -> {
+            writer.openBlock("if !restEncoder.HasHeader(\"Content-Type\") {", "}", () -> {
+                writer.write("restEncoder.SetHeader(\"Content-Type\").String($S)", getPayloadShapeMediaType(payloadShape));
+            });
+            writer.write("");
+
+            if (payloadShape.hasTrait(StreamingTrait.class)) {
+                writer.write("payload := $L", s);
+
+            } else if (payloadShape.isBlobShape()) {
+                writer.addUseImports(SmithyGoDependency.BYTES);
+                writer.write("payload := bytes.NewReader($L)", s);
+
+            } else if (payloadShape.isStringShape()) {
+                writer.addUseImports(SmithyGoDependency.STRINGS);
+                writer.write("payload := strings.NewReader(*$L)", s);
+
+            } else {
+                String functionName = ProtocolGenerator.getDocumentSerializerFunctionName(payloadShape,
+                        getProtocolName());
+                writer.addUseImports(SmithyGoDependency.SMITHY_XML);
+                writer.write("xmlEncoder := smithyxml.NewEncoder()");
+                writer.openBlock("if err := $L($L, xmlEncoder.Value); err != nil {", "}", functionName,
+                        s, () -> {
+                            writer.write("return out, metadata, &smithy.SerializationError{Err: err}");
+                        });
+                writer.write("payload := bytes.NewReader(xmlEncoder.Bytes())");
+            }
+
+            writer.openBlock("if request, err = request.SetStream(payload); err != nil {", "}",
+                    () -> {
+                        writer.write("return out, metadata, &smithy.SerializationError{Err: err}");
+                    });
+        });
     }
 
     @Override
@@ -99,12 +160,37 @@ abstract class RestXmlProtocolGenerator extends HttpBindingProtocolGenerator {
         shapes.forEach(shape -> shape.accept(visitor));
     }
 
+    /**
+     * Returns the MediaType for the payload shape derived from the MediaTypeTrait, shape type, or document content type.
+     *
+     * @param payloadShape shape bound to the payload.
+     * @return string for media type.
+     */
+    private String getPayloadShapeMediaType(Shape payloadShape) {
+        Optional<MediaTypeTrait> mediaTypeTrait = payloadShape.getTrait(MediaTypeTrait.class);
+
+        if (mediaTypeTrait.isPresent()) {
+            return mediaTypeTrait.get().getValue();
+        }
+
+        if (payloadShape.isBlobShape()) {
+            return "application/octet-stream";
+        }
+
+        if (payloadShape.isStringShape()) {
+            return "text/plain";
+        }
+
+        return getDocumentContentType();
+    }
+
+
 
     /*========================Deserializers==================================*/
-
     @Override
     protected void writeMiddlewareDocumentDeserializerDelegator(
-            GoWriter writer, Model model, SymbolProvider symbolProvider, OperationShape operation,
+            GenerationContext context,
+            OperationShape operation,
             GoStackStepMiddlewareGenerator generator
     ) {
 
@@ -127,5 +213,21 @@ abstract class RestXmlProtocolGenerator extends HttpBindingProtocolGenerator {
     @Override
     public ShapeId getProtocol() {
         return null;
+    }
+
+    @Override
+    protected void writeErrorMessageCodeDeserializer(GenerationContext context) {
+        // TODO: change this
+        context.getWriter().writeDocs("TODO: implement error message / code deser");
+        context.getWriter().addUseImports(SmithyGoDependency.FMT);
+        context.getWriter().write("fmt.Println(errorBody)");
+    }
+
+    @Override
+    protected void deserializeError(GenerationContext context, StructureShape shape) {
+        GoWriter writer = context.getWriter();
+
+        writer.write("return &smithy.DeserializationError{"
+                + "Err: fmt.Errorf(\"TODO: Implement error deserializerfor %v\", errorBody)}");
     }
 }
--- codegen/smithy-aws-go-codegen/src/main/java/software/amazon/smithy/aws/go/codegen/XmlShapeSerVisitor.java
+++ codegen/smithy-aws-go-codegen/src/main/java/software/amazon/smithy/aws/go/codegen/XmlShapeSerVisitor.java
@@ -1,4 +1,161 @@
 package software.amazon.smithy.aws.go.codegen;
 
-public class XmlShapeSerVisitor {
+import static software.amazon.smithy.go.codegen.integration.ProtocolUtils.writeSafeMemberAccessor;
+
+import java.util.Collections;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
+import java.util.TreeSet;
+import java.util.function.Predicate;
+import java.util.logging.Logger;
+import software.amazon.smithy.go.codegen.GoWriter;
+import software.amazon.smithy.go.codegen.integration.DocumentShapeSerVisitor;
+import software.amazon.smithy.go.codegen.integration.ProtocolGenerator.GenerationContext;
+import software.amazon.smithy.model.shapes.CollectionShape;
+import software.amazon.smithy.model.shapes.DocumentShape;
+import software.amazon.smithy.model.shapes.MapShape;
+import software.amazon.smithy.model.shapes.MemberShape;
+import software.amazon.smithy.model.shapes.Shape;
+import software.amazon.smithy.model.shapes.StructureShape;
+import software.amazon.smithy.model.shapes.UnionShape;
+import software.amazon.smithy.model.traits.EnumTrait;
+import software.amazon.smithy.model.traits.TimestampFormatTrait;
+import software.amazon.smithy.model.traits.XmlNameTrait;
+import software.amazon.smithy.utils.FunctionalUtils;
+
+final class XmlShapeSerVisitor extends DocumentShapeSerVisitor {
+ private static final TimestampFormatTrait.Format DEFAULT_TIMESTAMP_FORMAT = TimestampFormatTrait.Format.DATE_TIME;
+ private static final Logger LOGGER = Logger.getLogger(JsonShapeSerVisitor.class.getName());
+
+ private final Predicate<MemberShape> memberFilter;
+
+ public XmlShapeSerVisitor(
+         GenerationContext context
+ ) {
+  this(context, FunctionalUtils.alwaysTrue());
+ }
+
+ public XmlShapeSerVisitor(GenerationContext context,  Predicate<MemberShape> memberFilter) {
+  super(context);
+  this.memberFilter = memberFilter;
+ }
+
+ private XmlMemberSerVisitor getMemberSerVisitor(MemberShape member, String source, String dest) {
+  // Get the timestamp format to be used, defaulting to epoch seconds.
+  TimestampFormatTrait.Format format = member.getMemberTrait(getContext().getModel(), TimestampFormatTrait.class)
+          .map(TimestampFormatTrait::getFormat).orElse(DEFAULT_TIMESTAMP_FORMAT);
+  return new XmlMemberSerVisitor(getContext(), source, dest, format);
+ }
+
+ @Override
+ protected Map<String, String> getAdditionalSerArguments() {
+  return Collections.singletonMap("value", "smithyxml.Value");
+ }
+
+ @Override
+ protected void serializeCollection(GenerationContext context, CollectionShape shape) {
+  GoWriter writer = context.getWriter();
+  Shape target = context.getModel().expectShape(shape.getMember().getTarget());
+
+  writer.write("array, closeFn := value.Array()");
+  writer.write("defer closeFn()");
+  writer.write("");
+
+  writer.openBlock("for i := range v {", "}", () -> {
+   writer.write("av := array.NewMember()");
+
+   // Null values in lists should be serialized as such. Enums can't be null, so we don't bother
+   // putting this in for their case.
+   if (!target.hasTrait(EnumTrait.class)) {
+    writer.openBlock("if vv := v[i]; vv == nil {", "}", () -> {
+     writer.write("av.Null()");
+     writer.write("continue");
+    });
+   }
+
+   target.accept(getMemberSerVisitor(shape.getMember(), "v[i]", "av"));
+  });
+
+  writer.write("return nil");
+ }
+
+ @Override
+ protected void serializeDocument(GenerationContext context, DocumentShape shape) {
+  // TODO: implement document serialization
+  LOGGER.warning("Document type is currently unsupported for XML serialization.");
+  context.getWriter().writeDocs("TODO: implement document serialization.");
+  context.getWriter().write("return nil");
+ }
+
+ @Override
+ protected void serializeMap(GenerationContext context, MapShape shape) {
+  GoWriter writer = context.getWriter();
+  Shape target = context.getModel().expectShape(shape.getValue().getTarget());
+
+  writer.write("object, closeFn := value.Map()");
+  writer.write("defer closeFn()");
+  writer.insertTrailingNewline();
+
+  writer.openBlock("for key := range v {", "}", () -> {
+
+   writer.write("entry, closeFn := object.Entry()");
+   writer.write("defer closeFn()");
+   writer.write("om := entry.Key(key)");
+
+   // Null values in maps should be serialized as such. Enums can't be null, so we don't bother
+   // putting this in for their case.
+   if (!target.hasTrait(EnumTrait.class)) {
+    writer.openBlock("if vv := v[key]; vv == nil {", "}", () -> {
+     writer.write("om.Null()");
+     writer.write("continue");
+    });
+   }
+
+   target.accept(getMemberSerVisitor(shape.getValue(), "v[key]", "om"));
+  });
+
+  writer.write("return nil");
+ }
+
+ @Override
+ protected void serializeStructure(GenerationContext context, StructureShape shape) {
+  GoWriter writer = context.getWriter();
+
+  writer.write("object, closeFn := value.NestedElement()");
+  writer.write("defer closeFn()");
+  writer.insertTrailingNewline();
+
+  // Use a TreeSet to sort the members.
+  Set<MemberShape> members = new TreeSet<>(shape.getAllMembers().values());
+  for (MemberShape member : members) {
+   if (!memberFilter.test(member)) {
+    continue;
+   }
+   Shape target = context.getModel().expectShape(member.getTarget());
+   String serializedMemberName = getSerializedMemberName(member);
+   writeSafeMemberAccessor(context, member, "v", (operand) -> {
+    writer.write("ok := object.Key($S)", serializedMemberName);
+    target.accept(getMemberSerVisitor(member, operand, "ok"));
+   });
+   writer.write("");
+  }
+
+  writer.write("return nil");
+ }
+
+ @Override
+ protected void serializeUnion(
+         GenerationContext context, UnionShape shape
+ ) {
+  // TODO: implement document serialization
+  LOGGER.warning("Union type is currently unsupported for XML serialization.");
+  context.getWriter().writeDocs("TODO: implement union serialization.");
+  context.getWriter().write("return nil");
+ }
+
+ private String getSerializedMemberName(MemberShape memberShape) {
+  Optional<XmlNameTrait> xmlNameTrait = memberShape.getTrait(XmlNameTrait.class);
+  return xmlNameTrait.isPresent() ? xmlNameTrait.get().getValue() : memberShape.getMemberName();
+ }
 }
